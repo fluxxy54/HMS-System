dd178e876d9e7817c6b21fc4ff5dad834e99d839
fluxxy54 <ya9423561@gmail.com>
2025-12-14 18:16:27 +0200

    2 pages left

---
+++ build/classes/main/FullHistoryOfThePatient.rs
@@ -0,0 +1,3 @@
+com.mycompany.main.FullHistoryOfThePatient
+com.mycompany.main.FullHistoryOfThePatient$1
+com.mycompany.main.FullHistoryOfThePatient$2
---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,17 @@
+package Project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionProvider {
+
+    private static final String URL =
+        "jdbc:mysql://localhost:3306/isprojdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
